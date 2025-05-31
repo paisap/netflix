883595f7dbe3587665b5087ebcb4d3ab740533dc
root <root@DESKTOP-16HI4U1>
2025-05-30 21:42:09 -0500

    cors

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI, Depends, HTTPException
 from sqlalchemy.orm import Session
 from typing import List
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.database import SessionLocal, engine
 from app.models.usuario import Base, Usuario
@@ -16,3 +17,10 @@ app.include_router(cuenta.router)
 
 Base.metadata.create_all(bind=engine)  # Crea las tablas si no existen
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Puedes restringir esto por dominio, ej: ["https://tudominio.com"]
+    allow_credentials=True,
+    allow_methods=["*"],  # ["GET", "POST", "PUT", "DELETE"]
+    allow_headers=["*"],
+)
